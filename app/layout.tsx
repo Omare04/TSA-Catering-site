348d10d64721a085e383a11ad2bfafcd525e7d28
Omar Elmasaoudi <omarelmasaoudi04@gmail.com>
2024-08-19 11:52:40 -0400

    final changes added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/images/favicon.ico" sizes="any" />
+      </head>
+
       <body className={inter.className}>{children}</body>
     </html>
   );
